6ee0772fdc0eb867d031e07c618043d336498579
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-19 15:18:31 +0200

    refactor: move hashes to a separate nix file
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- pkgs/dagger/default.nix
+++ pkgs/dagger/default.nix
@@ -16,12 +16,7 @@ stdenv.mkDerivation rec {
         aarch64-linux = "linux_arm64";
         aarch64-darwin = "darwin_arm64";
       };
-      hash = selectSystem {
-        x86_64-linux = "sha256-q/QO1WGbdB3S1e4Sv8/vDkHdSoJa187gnkawBwbniZM=";
-        x86_64-darwin = "sha256-klRgcToFKnfNod2BIbs9x7c/Keg70r+D8N4Xvg7uFZk=";
-        aarch64-linux = "sha256-juVqvLWdP2WGPr2q+tHaMjgdM7yEAGErzbE6+ci4128=";
-        aarch64-darwin = "sha256-YmcPnhqZlNr+7kjFXqRRSbRPFWxVZCd8rs6mKZCw4KY=";
-      };
+      hash = selectSystem (import ./hashes.nix);
     in
     fetchurl {
       inherit hash;
